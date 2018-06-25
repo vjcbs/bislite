215fb3bfc0f0b04f3c95ffe1db645e0d42ccad9b
owlish <vaynimble@gmail.com>
2018-06-25 15:29:47 +0600

    Sprites, popup on gallery images

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,21 @@
 $(document).ready( function() {
-  let dropdown = $('.dropdown-toggle');
-  console.log(dropdown);
+
+  let menuToggler = $('.navbar-toggler');
+  let menuTogglerIcon = $('.navbar-toggler-icon');
+
+  menuToggler.click( function(e) {
+    if ($(this).attr('aria-expanded') == 'true') {
+      menuTogglerIcon.css('background', 'url("../img/icon-menu-open.svg") no-repeat center');
+    } else {
+      menuTogglerIcon.css('background', 'url("../img/icon-menu-close.svg") no-repeat center');
+    }
+  });
+
+  let filterButtons = $('.portfolio__btn-group .btn');
+
+  filterButtons.click(function (e) {
+    filterButtons.removeClass('btn-active');
+    // e.target.classList.add('btn-active');
+    $(this).addClass('btn-active');
+  })
 })
